98b3daeaec5c7202f8eb2985d51afd58fbfcbb34
mdmahboobalam <mdmahboobalam450@gmail.com>
2026-01-07 17:14:32 +0530

    commit

--- JavaScript/Day_6/app.js
+++ JavaScript/Day_6/app.js
@@ -1,78 +1,78 @@
-// // Questions
-// // write a function sayHello() that prints "Hello js"
+// Questions
+// write a function sayHello() that prints "Hello js"
 
-// function sayHello(){
-//     console.log("hello js")
-// }
-// sayHello()
+function sayHello(){
+    console.log("hello js")
+}
+sayHello()
 
-// // create a function add(a,b) that returns their sum and log the result
+// create a function add(a,b) that returns their sum and log the result
 
-// let sum=(a,b)=>{
-//     return a+b
-// }
+let sum=(a,b)=>{
+    return a+b
+}
 
-// console.log(sum(2,4))
+console.log(sum(2,4))
 
-// // write a function with a default parameter name="Guest" that prints "Hi <name>"
+// write a function with a default parameter name="Guest" that prints "Hi <name>"
 
-// function fun(name="Guest"){
-//     console.log(`Hi ${name}`)
-// }
-// fun()
+function fun(name="Guest"){
+    console.log(`Hi ${name}`)
+}
+fun()
 
-// // use rest parameter to make a function that unlimited numbers.
+// use rest parameter to make a function that unlimited numbers.
 
-// function add(...a){
-//     let sum=0
-//     a.forEach(function(val){
-//        sum+=val
-//     })
-//     console.log(sum)
-// }
-// add(1,2,3,4,4,5,6,7,7,8)
-// console.log(sm)
+function add(...a){
+    let sum=0
+    a.forEach(function(val){
+       sum+=val
+    })
+    console.log(sum)
+}
+add(1,2,3,4,4,5,6,7,7,8)
+console.log(sm)
 
-// function addUnlimited(...nums){
-//     let ans=nums.reduce(function(acc,val){
-//         return acc+val;
-//     },0);
-//     console.log(ans);
-// }
+function addUnlimited(...nums){
+    let ans=nums.reduce(function(acc,val){
+        return acc+val;
+    },0);
+    console.log(ans);
+}
 
-// addUnlimited(1,2,3);
+addUnlimited(1,2,3);
 
-// // create an IIFE(immediatly invoked function expression) that prints "I run instantly"
+// create an IIFE(immediatly invoked function expression) that prints "I run instantly"
 
-// (function(){
-//     console.log("I run instantly")
-// })();
+(function(){
+    console.log("I run instantly")
+})();
 
-// // make a nested function where the inner one prints a variable from the outer one.
-// function outer(){
-//     let x=12
-//    function inner(){
-//      console.log(x);
-//    }
-//    inner()
-// }
+// make a nested function where the inner one prints a variable from the outer one.
+function outer(){
+    let x=12
+   function inner(){
+     console.log(x);
+   }
+   inner()
+}
 
-// outer()
+outer()
 
-// // create a array of 5 fruits . add one at the end and remove one from the beginning.
-// let arr=[1,2,3,4,5,6]
-// arr.push(19)
-// arr.shift()
+// create a array of 5 fruits . add one at the end and remove one from the beginning.
+let arr=[1,2,3,4,5,6]
+arr.push(19)
+arr.shift()
 
-// // use a for loop to print all elements of an arar.
+// use a for loop to print all elements of an arar.
 
-// let ab=[1,2,3,4,5,5,6,77,7]
+let ab=[1,2,3,4,5,5,6,77,7]
 
-// ab.forEach(element => {
-//     console.log(element)
-// });
+ab.forEach(element => {
+    console.log(element)
+});
 
-// // create an object person with keys name, age , and city,and print each key's value.
+// create an object person with keys name, age , and city,and print each key's value.
 
 let person={
     name:"mahboob",
